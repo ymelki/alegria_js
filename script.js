e2f8ed4e9066cf52d7477ddae6b9d6582509ecc2
Yoel Melki <ymelki@gmail.com>
2022-09-13 15:24:39 +0200

    first commit

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ console.log("j'écris dans la log ! ");
 Les variables :
 stocké de l'information
 variable numerique
+
+concatenation : 
 */
-prix_telephone=1200;
+prix_telephone=1200; // affectation de variable
 prix_ttc_telephone=prix_telephone*1.2;
 console.log(prix_telephone);
 console.log(prix_ttc_telephone);
@@ -16,5 +18,29 @@ console.log(prix_ttc_telephone);
 // AFFICHER DANS LA LOG LE MONTANT DE LA TVA DE LA VARIABLE prix_telephone
 TVA=prix_ttc_telephone-prix_telephone;
 console.log(TVA);
+
+console.log("Le prix du Téléphone est de " + prix_telephone);
+
+
+// inversion de variables
+SEL  =100;
+SUCRE=120;
+
+console.log("Nous avons du Sel  " + SEL);
+console.log("Nous avons du Sucre  " + SUCRE);
+
+/***  Ne pas toucher a ce qui est au dessus   */
+
+// votre code qui va inverser les variables
+
+
+
+/***  Ne pas toucher a ce qui est en dessous   */
+
+
+console.log("Nous avons du Sel  " + SEL);
+console.log("Nous avons du Sucre  " + SUCRE);
+
+
 
 

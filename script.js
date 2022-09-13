a87c18d9ee1d5fc54fee7a47c7671f8bd79e24fc
Yoel Melki <ymelki@gmail.com>
2022-09-13 15:10:56 +0200

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("j'écris dans la log ! ")
+console.log("j'écris dans la log ! ");
+
+// du commentaire
+
+/*
+
+Les variables :
+stocké de l'information
+variable numerique
+*/
+prix_telephone=1200;
+prix_ttc_telephone=prix_telephone*1.2;
+console.log(prix_telephone);
+console.log(prix_ttc_telephone);
+
+// AFFICHER DANS LA LOG LE MONTANT DE LA TVA DE LA VARIABLE prix_telephone
+

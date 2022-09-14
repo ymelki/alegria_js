d87319f7a639305693b79183ed84558cc5092812
Yoel Melki <ymelki@gmail.com>
2022-09-14 17:07:17 +0200

    first commit

--- script2.js
+++ script2.js
@@ -2,8 +2,18 @@
 
 // CREER UNE FONCTION QUI VA CREER UNE VARIABLE PRENOM ET QUI AFFICHE VOTRE PRENOM  !
 function affiche_hello()  {
-    console.log("Hello ! ");
+    let prenom="Yoel"
+    console.log("Hello ! " + prenom);
 }
 
 // appel de fonction
 affiche_hello();
+
+
+function afficher_prenom( toto  )  {
+    console.log("Hello ! " + toto);
+}
+
+// fonction qui va prendre un parametre une chaine de caractere qui sera un nom et qui l affichera
+afficher_prenom("Axel");
+afficher_prenom("Melki")

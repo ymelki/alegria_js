810e0d0a460bcfd028ca62b3e8b9b9a2c6d8a631
Yoel Melki <ymelki@gmail.com>
2022-09-15 17:21:35 +0200

    first commit

---
+++ script4.js
@@ -0,0 +1,5 @@
+document.getElementById("monbouton").addEventListener("click" , mafonction);
+
+function mafonction(){
+document.getElementById("madiv").innerHTML="montexte change au moment du chargement de la page ! ";
+}

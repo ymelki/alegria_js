cfc33be50c97890ebe3e747004300242afbb15f3
Yoel Melki <ymelki@gmail.com>
2022-09-15 17:42:41 +0200

    first commit

--- script4.js
+++ script4.js
@@ -1,5 +1,10 @@
+// evenement qui lance une fonction
 document.getElementById("monbouton").addEventListener("click" , mafonction);
 
+// fonction affichant l'information
 function mafonction(){
-document.getElementById("madiv").innerHTML="montexte change au moment du chargement de la page ! ";
+document.getElementById("madiv").innerHTML= parseInt(document.getElementById("madiv").innerHTML) + 1;
 }
+
+// EXO : dans la div par defaut on a 1 . 
+// bouton GO => dans la div 2 .. 3 .. 4. ..

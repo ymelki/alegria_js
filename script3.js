24c899da5f708ed83dd508f12a6ad5e23819cf18
Yoel Melki <ymelki@gmail.com>
2022-09-15 14:39:39 +0200

    first commit

--- script3.js
+++ script3.js
@@ -51,6 +51,7 @@ let alea=Math.floor(Math.random() * 100);
 
 
 // creer une fonction qui renvoie vrai ou faux 
+/*
 function isequal(nb){
     if (nb == alea){
         return true;
@@ -83,7 +84,60 @@ function niveau(nb_chance){
 
 niveau(10);
 niveau(3);
+*/
 
+// PIERRE CISEAU 
+
+let tableau = ["pierre", "ciseau", "papier"];
+// Joueur 1
+let j1=Math.floor(Math.random() * 3);
+
+// joueur 2
+let j2=Math.floor(Math.random() * 3);
+console.log(j1);
+console.log(j2);
+
+function test(tab){
+    console.table(tab);
+    return tab;
+} 
+
+function check(tableau){
+    tableau[0]
+    
+}
+
+//parametre tableau[valeurj1 , valeurj2]
+
+// on envoie deux indices 
+let joueur = [j1,j2]
+test(joueur); 
+
+let var1="Magnifique ! ";
+result="";
+console.log(var1);
+for (let i=var1.length-1; i >= 0; i--){
+    console.log(i);
+    console.log(var1[i]);
+    result=result+var1[i]
+
+}
+
+function reverse2(variable){
+    result="";
+    console.log(variable);
+    for (let i=variable.length-1; i >= 0; i--){
+        console.log(i);
+        console.log(variable[i]);
+        result=result+variable[i]
+
+    }
+
+}
+reverse2("Henri !")
+reverse2("Pierre !")
+reverse2("Jean !")
+reverse2("??? !")
 // GENERER UN NOMBRE ALEATOIRE AVANT
 // BOUCLE TANT QUE < 6 CHANCES
 /** IF LE NOMBRE EST = , > ou < 
